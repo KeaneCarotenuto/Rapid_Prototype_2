416b7bc98a6856c7327e441527e3f23beb892991
Nerys-Thamm <nerysthamm@gmail.com>
2021-08-13 15:10:32 +1200

    worked on task system

--- Assets/Scripts/TaskData.cs
+++ Assets/Scripts/TaskData.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TaskData : MonoBehaviour
+public class TaskData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string m_TaskID;
+    public string m_TaskDesc;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float m_TaskDuration;
+
+    public bool m_SpawnsItems;
+
+    public string m_ItemTag;
+
+    public List<GameObject> m_TaskItems;
+
+    public List<GameObject> m_SpawnedItems;
 }

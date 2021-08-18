9ab866e259a9a2767ab4a5710adb0071f2da5496
Keane <KeaneCarotenuto@gmail.com>
2021-08-18 20:28:32 +1200

    Working on plant task

--- Assets/Scripts/Tasks/PlantTask.cs
+++ Assets/Scripts/Tasks/PlantTask.cs
@@ -8,22 +8,34 @@ public class PlantTask : Task
     public Transform meatPosition;
     public float spawnHeight = 10;
 
+    [SerializeField] Animator anim;
+
     public override void StartTask(float _time = Mathf.Infinity)
     {
         if (taskExists) return;
         startTime = Time.time;
         taskTime = _time;
 
+        anim.SetBool("Idling", false);
+        anim.SetBool("Waiting", true);
+        anim.SetBool("Eating", false);
+
         if (!m_currentMeat) m_currentMeat = Instantiate(MeatPrefab, meatPosition.position + new Vector3(0, spawnHeight, 0), Quaternion.identity, transform);
         Meat meat = m_currentMeat.GetComponent<Meat>();
         meat.completed.AddListener(CompleteTask);
-        meat.failed.AddListener(() => { FailTask(); });
+        meat.failed.AddListener(() => { 
+            FailTask(); 
+        });
         TaskFailed.AddListener(() => {
             if (m_currentMeat)
             {
                 Destroy(m_currentMeat);
                 m_currentMeat = null;
             }
+
+            anim.SetBool("Idling", false);
+            anim.SetBool("Waiting", true);
+            anim.SetBool("Eating", false);
         });
 
         taskExists = true;
